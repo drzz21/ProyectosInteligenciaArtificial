a1c8d4275b9d43ded4ed9c50dece35d6fff82e35
Francisco Diosdado <drz.y2k@gmail.com>
2018-03-17 22:56:35 -0600

    Add files via upload

---
+++ productointernovectores_s14120072.py
@@ -0,0 +1,17 @@
+vectora=[-5,4]
+vectorb=[-3,-8]
+
+
+
+crucea=(vectora[0]*vectorb[0])
+cruceb=(vectora[1]*vectorb[1])
+
+resultadofinal=crucea+cruceb
+
+print(" a = ",vectora)
+print(" b = ",vectorb)
+print(" a . b = (",vectora[0]," * ",vectorb[0],") + (",vectora[1]," * ",vectorb[1],")")
+print(" a . b = (",crucea,") + (",cruceb,")")
+print(" a . b = ",resultadofinal)
+
+
